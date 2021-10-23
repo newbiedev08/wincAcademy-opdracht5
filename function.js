5d02970bfa68fcf63e1a13613e994c8add28ed27
Ivy Kathrina <88631468+newbiedev08@users.noreply.github.com>
2021-10-23 10:14:35 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+
+//writing a function//
+const pizzaJalapeno = function () {
+
+    console.log('Make a pizza dough');
+    console.log('Put your favorite toppings on pie');
+    console.log('Bake pizza in de over for 15 minutes');
+}
+pizzaJalapeno();
+pizzaJalapeno();
+
+const sushiMaki = function(){
+     
+    console.log('Make a sushi rice');
+    console.log('Spread the rice on the Nori');
+    console.log('Add filling on the Nori');
+    console.log('Roll the sushi');
+    console.log('Cut the sushi');
+    }
+
+sushiMaki();
+sushiMaki();
+sushiMaki();
